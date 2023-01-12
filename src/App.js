47c1d156f4782920c40eaca5ef6cd32536d9f6b1
emilyannemoses <e.a.moses@gmail.com>
2023-01-11 19:46:00 -0600

    save changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './athena.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is Emily's first React App.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://emilyannemoses.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out my portfolio!
         </a>
       </header>
     </div>
